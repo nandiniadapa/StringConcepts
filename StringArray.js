90449cf432d7530498a905c1c2e762372b13206d
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-03 16:07:07 +0530

    stringarrays.html

--- StringArray.js
+++ StringArray.js
@@ -4,5 +4,6 @@ document.getElementById("btnPrint").addEventListener("click",function(){
     document.getElementById("pResult").innerHTML=name[1];
     document.getElementById("pLength").innerHTML=name.length;
     document.getElementById("pLastletter").innerHTML=name[name.length-1];
+     document.getElementById("Result").innerHTML=name[0]+","+name[1]+" ,"+name[2]+","+name[3]+","+name[4]+",";
 
 });
