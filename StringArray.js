54772c8c97588ff6681a80784a39f3a177439f28
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-03 15:38:49 +0530

    stringarrays

--- StringArray.js
+++ StringArray.js
@@ -1,6 +1,8 @@
 document.getElementById("btnPrint").addEventListener("click",function(){
     debugger;
     var name=document.getElementById("txtName").value
-    document.getElementById("pResult").innerHTML=name[0];
+    document.getElementById("pResult").innerHTML=name[1];
+    document.getElementById("pLength").innerHTML=name.length;
+    document.getElementById("pLastletter").innerHTML=name[name.length-1];
 
 });
