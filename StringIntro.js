4fd4a31011051e65c0319239616c32d0e3f63328
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-03 11:44:32 +0530

    feat(StringIntro.js):linking with js

---
+++ StringIntro.js
@@ -0,0 +1,10 @@
+document.getElementById("btnSubmit").addEventListener("click",function(){
+    debugger;
+   var firstname=document.getElementById("txtfirstname").value
+   var Lastname=document.getElementById("txtLastname").value
+   var Email=document.getElementById("txtEmail").value
+    
+   alert(firstname);
+   alert(Lastname);
+   alert(Email);
+});
