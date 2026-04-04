085d20ff41c4a8f5ddacae9e0041a1c6abc0baf6
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-04 12:18:37 +0530

    email comparison

--- EmailComparison.js
+++ EmailComparison.js
@@ -1,1 +1,11 @@
-docy
+document.getElementById("btnEmail").addEventListener("click", function(){
+    debugger;
+    var email=document.getElementById("txtEmail").value;
+    if( email.toLowerCase().trim()=="nnandiniadapa@gmail.com".toLowerCase().trim()){
+        document.getElementById("pResult").innerHTML="Login succesfully.";
+    }else{
+        document.getElementById("pResult").innerHTML="Login failed.please check your email.";
+    }
+});
+
+    
