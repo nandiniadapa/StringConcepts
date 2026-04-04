91c6b4e5dc5c2c23a432837967841143a997729b
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-04 13:11:33 +0530

    StringSlice.js

---
+++ StringSlice.js
@@ -0,0 +1,12 @@
+
+document.getElementById("btnString"), addEventListener("click", function () {
+    debugger;
+    var name = document.getElementById("txtName").value;
+    var startNumber = Number(document.getElementById("txtStartNumber").value);
+    var endNumber = Number(document.getElementById("txtEndNumber").value);
+    document.getElementById("pResult").innerHTML = name.slice(startNumber, endNumber);
+
+    // " S o w j i"
+    //   0 1 2 3 4 
+
+});
