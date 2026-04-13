c03e1511f56ac035afc6d723516436029f236ec9
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-13 21:14:31 +0530

    StringEndsWith.js

---
+++ StringEndsWith.js
@@ -0,0 +1,6 @@
+document.getElementById("btnFind").addEventListener("click",function(){
+    debugger;
+  var stringName=document.getElementById("txtStringName").value
+    var search=document.getElementById("txtSearch").value
+document.getElementById("pResult").innerHTML=stringName.endsWith(search)
+     });
